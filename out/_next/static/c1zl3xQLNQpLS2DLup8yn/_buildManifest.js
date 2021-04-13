c35ab68366fbc186803cf4f8a65b04206bbc9bd1
Slaven Bacic <bacic.slaven@gmail.com>
2021-04-13 11:12:47 +0200

    chore: add updated build

---
+++ out/_next/static/c1zl3xQLNQpLS2DLup8yn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-9075c235d57534b23132.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5302ec098c437a0527f2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
